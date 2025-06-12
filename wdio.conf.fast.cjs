88dab497e76ae89fcd4c8e1edde549e97d032c7f
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2025-06-12 03:27:33 -0300

    appium

--- wdio.conf.fast.cjs
+++ wdio.conf.fast.cjs
@@ -1,30 +1,35 @@
-// Usando Appium para pruebas móviles
-// Si quiere TS, puede usar ts-node, pero specs son JS
-
 exports.config = {
     runner: 'local',
     hostname: 'localhost',
     port: 4723,
-    path: '/wd/hub',
+    path: '/',
     specs: ['./e2e/appium/**/*.js'],
     maxInstances: 1,
     capabilities: [{
         platformName: 'Android',
         'appium:automationName': 'UiAutomator2',
         'appium:udid': 'emulator-5554',
-        'appium:deviceName': 'Medium Phone API 35',
+        'appium:deviceName': 'Appium_Test_Device',
         'appium:platformVersion': '13.0',
-        browserName: 'Chrome'
+        browserName: 'Chrome',
+        'appium:chromedriverAutodownload': true,
+        'goog:chromeOptions': {
+            args: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-web-security']
+        }
     }],
-    logLevel: 'info',
+    logLevel: 'warn',
     bail: 0,
     baseUrl: 'http://10.0.2.2:3000',
-    waitforTimeout: 10000,
-    services: [],  // no se arranca Appium localmente
+    waitforTimeout: 5000,
+    services: [],
     framework: 'mocha',
     reporters: ['spec'],
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 30000
+    },
+    
+    before: function (capabilities, specs) {
+        browser.setTimeout({ 'implicit': 3000 });
     }
-}; 
+};
